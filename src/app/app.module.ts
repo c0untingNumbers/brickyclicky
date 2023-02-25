e7dbd4e3c2570be0fe75c936685ecd0397be0fe3
crazyforMcCafe <rayhmiot@gmail.com>
2023-02-25 15:02:42 -0500

    added brick component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrickComponent } from './brick/brick.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrickComponent
   ],
   imports: [
     BrowserModule
