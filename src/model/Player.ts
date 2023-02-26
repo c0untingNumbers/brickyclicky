368cc0973a337f0fa658be1c0a4243b472afce84
crazyforMcCafe <rayhmiot@gmail.com>
2023-02-26 02:12:03 -0500

    updated work rate in Player

--- src/model/Player.ts
+++ src/model/Player.ts
@@ -6,11 +6,11 @@ import { Structure } from './Structure';
 class Player {
   private numBricks: number = 0;
 
-  click(): void{
+  click(): void {
     this.numBricks++;
-  } 
+  }
   work(): void {
-    const income = this.getTotalEfficiency();
+    const income = this.getTotalEfficiency() / 60;
     this.numBricks += income;
   }
 
@@ -21,10 +21,10 @@ class Player {
   }
 
   moveWorkers(to: Structure, from: Structure, amount: number): boolean {
-    if(!from.removeWorkers(amount)){
+    if (!from.removeWorkers(amount)) {
       return false;
     }
-    if(!to.addWorkers(amount)){
+    if (!to.addWorkers(amount)) {
       from.addWorkers(amount);
       return false;
     }
@@ -35,7 +35,7 @@ class Player {
     return this.numBricks;
   }
 
-  spend(amount: number): void{
+  spend(amount: number): void {
     this.numBricks -= amount;
   }
 }
