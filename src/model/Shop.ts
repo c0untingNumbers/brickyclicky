dd79a1dfd12214c751a41f67fcf90bcb0f965714
tsg1472 <tsg1472@rit.edu>
2023-02-26 00:34:15 -0500

    Econony changes

--- src/model/Shop.ts
+++ src/model/Shop.ts
@@ -4,23 +4,43 @@ import Land from './Land';
 import Player from './Player';
 
 export class Shop {
+  private apartmentCost: number = 200;
+  private workerCost: number = 50;
+  private factoryCost: number = 1000;
+
   upgradeApartment(): boolean {
-    if (Player.getNumBricks() < 200) return false;
-    Player.spend(200);
+    if (Player.getNumBricks() < this.apartmentCost) return false;
+    Player.spend(this.apartmentCost);
+    this.apartmentCost *= 0.1; 
     return Apartment.expand();
   }
 
   upgradeFactory(): boolean {
-    if (Player.getNumBricks() < 1000) return false;
-    Player.spend(1000);
+    if (Player.getNumBricks() < this.factoryCost) return false;
+    Player.spend(this.factoryCost);
+    this.factoryCost *= 0.1;
     return Factory.expand();
   }
 
   buyWorker(): boolean {
-    if (Player.getNumBricks() < 50) return false;
-    Player.spend(50);
+    if (Player.getNumBricks() < this.workerCost) return false;
+    Player.spend(this.workerCost);
+    this.workerCost *= 0.1;
     return Land.addWorkers(1);
+  }
+
+  getWorkerCost(): number{
+    return this.workerCost;
+  }
+
+  getApartmentCost(): number{
+    return this.apartmentCost;
+  }
+
+  getFactoryCost(): number{
+    return this.factoryCost;
   }
 }
 
-export default new Shop();
+export default new Shop(
+);
