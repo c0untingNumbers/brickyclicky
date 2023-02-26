8cc7bd8931321be21203ed9be44c6d8cc732e96c
tsg1472 <tsg1472@rit.edu>
2023-02-25 21:52:55 -0500

    Removed duplicate file

--- src/app/structure.ts
+++
@@ -1,9 +0,0 @@
-export interface Structure {
-
-    getNumWorkers(): number;
-    addWorkers(workers: number): boolean;
-    removeWorkers(workers: number): boolean;
-    getEfficiency(): number;
-    getCapacity(): number;
-    expand(): boolean;
-}
