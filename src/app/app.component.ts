3508f8d70608b5dbdd2b6488dce996dfe6b0fffb
crazyforMcCafe <rayhmiot@gmail.com>
2023-02-25 19:36:22 -0500

    setup pixi.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'brickyclicky';
   
+  private app = new PIXI.Application<HTMLCanvasElement>({
+    width: innerWidth,
+    height: innerHeight,
+  });
+
+  ngOnInit(): void {
+    document.body.appendChild(this.app.view);
+  }
 }
